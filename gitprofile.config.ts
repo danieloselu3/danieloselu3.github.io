faed089e36e16f8f8e7e4146cbbb044f3da46209
danieloselu3 <doselu3@gmail.com>
2024-02-27 11:14:11 +0300

    changed project display from automatic to manual

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -14,7 +14,7 @@ const CONFIG = {
     github: {
       display: true, // Display GitHub projects?
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'manual', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'updated', // Sort projects by 'stars' or 'updated'
         limit: 4, // How many projects to display.
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        // projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['danieloselu3/SyriaTel-Telecom-Analysis', 'danieloselu3/Mobile-Phone-Sentiment-Analysis', 'danieloselu3/Linear-Regression-Project', 'danieloselu3/Twitter-Sentiment-Analysis'] // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
