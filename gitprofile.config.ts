05ad227b39ff84f584d95229dea8b1f65ef0b361
danieloselu3 <doselu3@gmail.com>
2024-02-28 09:47:29 +0300

    added blog handle

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -131,8 +131,8 @@ const CONFIG = {
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: 'medium', // medium | dev
+    username: 'doselu3', // to hide blog section, keep it empty
     limit: 3, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
@@ -159,9 +159,9 @@ const CONFIG = {
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
-      // 'light',
-      // 'dark',
-      // 'cupcake',
+      'light',
+      'dark',
+      'cupcake',
       // 'bumblebee',
       // 'emerald',
       // 'corporate',
